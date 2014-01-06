432f52e2851d55c8bc0be8e8bf7c5b0ce3b28ce2
explorer14 <amanag1983@gmail.com>
2014-01-06 22:24:09 +0000

    Initial 2

---
+++ ChangePackBuilder/frmChangePackBuilder.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace ChangePackBuilder
+{
+    partial class frmChangePackBuilder
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmChangePackBuilder));
+            this.changeTracker = new System.IO.FileSystemWatcher();
+            this.btnMonitor = new System.Windows.Forms.Button();
+            this.changeList = new System.Windows.Forms.ListBox();
+            this.rightClickMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.removeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnBuildReleasePack = new System.Windows.Forms.Button();
+            this.ReleaseFolderBrowser = new System.Windows.Forms.FolderBrowserDialog();
+            this.txtPathToMonitor = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSelectPathToMonitor = new System.Windows.Forms.Button();
+            this.btnBrowseToReleaseDestination = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtReleaseDestination = new System.Windows.Forms.TextBox();
+            this.btnStopMonitoring = new System.Windows.Forms.Button();
+            this.btnClearList = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.changeTracker)).BeginInit();
+            this.rightClickMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // changeTracker
+            // 
+            this.changeTracker.EnableRaisingEvents = true;
+            this.changeTracker.IncludeSubdirectories = true;
+            this.changeTracker.SynchronizingObject = this;
+            // 
+            // btnMonitor
+            // 
+            this.btnMonitor.Location = new System.Drawing.Point(16, 349);
+            this.btnMonitor.Name = "btnMonitor";
+            this.btnMonitor.Size = new System.Drawing.Size(118, 23);
+            this.btnMonitor.TabIndex = 0;
+            this.btnMonitor.Text = "Start Monitoring";
+            this.btnMonitor.UseVisualStyleBackColor = true;
+            this.btnMonitor.Click += new System.EventHandler(this.btnMonitor_Click);
+            // 
+            // changeList
+            // 
+            this.changeList.ContextMenuStrip = this.rightClickMenu;
+            this.changeList.FormattingEnabled = true;
+            this.changeList.HorizontalScrollbar = true;
+            this.changeList.Location = new System.Drawing.Point(13, 44);
+            this.changeList.Name = "changeList";
+            this.changeList.Size = new System.Drawing.Size(630, 225);
+            this.changeList.TabIndex = 1;
+            // 
+            // rightClickMenu
+            // 
+            this.rightClickMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.removeToolStripMenuItem});
+            this.rightClickMenu.Name = "rightClickMenu";
+            this.rightClickMenu.Size = new System.Drawing.Size(118, 26);
+            // 
+            // removeToolStripMenuItem
+            // 
+            this.removeToolStripMenuItem.Name = "removeToolStripMenuItem";
+            this.removeToolStripMenuItem.Size = new System.Drawing.Size(117, 22);
+            this.removeToolStripMenuItem.Text = "Remove";
+            this.removeToolStripMenuItem.Click += new System.EventHandler(this.removeToolStripMenuItem_Click);
+            // 
+            // btnBuildReleasePack
+            // 
+            this.btnBuildReleasePack.Location = new System.Drawing.Point(264, 349);
+            this.btnBuildReleasePack.Name = "btnBuildReleasePack";
+            this.btnBuildReleasePack.Size = new System.Drawing.Size(118, 23);
+            this.btnBuildReleasePack.TabIndex = 2;
+            this.btnBuildReleasePack.Text = "Build Change Pack";
+            this.btnBuildReleasePack.UseVisualStyleBackColor = true;
+            this.btnBuildReleasePack.Click += new System.EventHandler(this.btnBuildReleasePack_Click);
+            // 
+            // txtPathToMonitor
+            // 
+            this.txtPathToMonitor.Location = new System.Drawing.Point(126, 278);
+            this.txtPathToMonitor.Name = "txtPathToMonitor";
+            this.txtPathToMonitor.ReadOnly = true;
+            this.txtPathToMonitor.Size = new System.Drawing.Size(299, 20);
+            this.txtPathToMonitor.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 282);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Path to Monitor:";
+            // 
+            // btnSelectPathToMonitor
+            // 
+            this.btnSelectPathToMonitor.Location = new System.Drawing.Point(431, 277);
+            this.btnSelectPathToMonitor.Name = "btnSelectPathToMonitor";
+            this.btnSelectPathToMonitor.Size = new System.Drawing.Size(36, 23);
+            this.btnSelectPathToMonitor.TabIndex = 5;
+            this.btnSelectPathToMonitor.Text = "...";
+            this.btnSelectPathToMonitor.UseVisualStyleBackColor = true;
+            this.btnSelectPathToMonitor.Click += new System.EventHandler(this.btnSelectPathToMonitor_Click);
+            // 
+            // btnBrowseToReleaseDestination
+            // 
+            this.btnBrowseToReleaseDestination.Location = new System.Drawing.Point(431, 303);
+            this.btnBrowseToReleaseDestination.Name = "btnBrowseToReleaseDestination";
+            this.btnBrowseToReleaseDestination.Size = new System.Drawing.Size(36, 23);
+            this.btnBrowseToReleaseDestination.TabIndex = 8;
+            this.btnBrowseToReleaseDestination.Text = "...";
+            this.btnBrowseToReleaseDestination.UseVisualStyleBackColor = true;
+            this.btnBrowseToReleaseDestination.Click += new System.EventHandler(this.btnBrowseToReleaseDestination_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 308);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Release Destination :";
+            // 
+            // txtReleaseDestination
+            // 
+            this.txtReleaseDestination.Location = new System.Drawing.Point(126, 304);
+            this.txtReleaseDestination.Name = "txtReleaseDestination";
+            this.txtReleaseDestination.ReadOnly = true;
+            this.txtReleaseDestination.Size = new System.Drawing.Size(299, 20);
+            this.txtReleaseDestination.TabIndex = 6;
+            // 
+            // btnStopMonitoring
+            // 
+            this.btnStopMonitoring.Location = new System.Drawing.Point(140, 349);
+            this.btnStopMonitoring.Name = "btnStopMonitoring";
+            this.btnStopMonitoring.Size = new System.Drawing.Size(118, 23);
+            this.btnStopMonitoring.TabIndex = 9;
+            this.btnStopMonitoring.Text = "Stop Monitoring";
+            this.btnStopMonitoring.UseVisualStyleBackColor = true;
+            this.btnStopMonitoring.Click += new System.EventHandler(this.btnStopMonitoring_Click);
+            // 
+            // btnClearList
+            // 
+            this.btnClearList.Location = new System.Drawing.Point(525, 276);
+            this.btnClearList.Name = "btnClearList";
+            this.btnClearList.Size = new System.Drawing.Size(118, 23);
+            this.btnClearList.TabIndex = 10;
+            this.btnClearList.Text = "Clear List";
+            this.btnClearList.UseVisualStyleBackColor = true;
+            this.btnClearList.Click += new System.EventHandler(this.btnClearList_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 18);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Changed Files";
+            // 
+            // frmChangePackBuilder
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(657, 383);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnClearList);
+            this.Controls.Add(this.btnStopMonitoring);
+            this.Controls.Add(this.btnBrowseToReleaseDestination);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtReleaseDestination);
+            this.Controls.Add(this.btnSelectPathToMonitor);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPathToMonitor);
+            this.Controls.Add(this.btnBuildReleasePack);
+            this.Controls.Add(this.changeList);
+            this.Controls.Add(this.btnMonitor);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmChangePackBuilder";
+            this.Text = "ChangePackBuilder ver ";
+            this.Load += new System.EventHandler(this.frmReleaseBuilder_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.changeTracker)).EndInit();
+            this.rightClickMenu.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.FileSystemWatcher changeTracker;
+        private System.Windows.Forms.Button btnMonitor;
+        private System.Windows.Forms.ListBox changeList;
+        private System.Windows.Forms.Button btnBuildReleasePack;
+        private System.Windows.Forms.FolderBrowserDialog ReleaseFolderBrowser;
+        private System.Windows.Forms.Button btnSelectPathToMonitor;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPathToMonitor;
+        private System.Windows.Forms.Button btnBrowseToReleaseDestination;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtReleaseDestination;
+        private System.Windows.Forms.Button btnStopMonitoring;
+        private System.Windows.Forms.Button btnClearList;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ContextMenuStrip rightClickMenu;
+        private System.Windows.Forms.ToolStripMenuItem removeToolStripMenuItem;
+    }
+}
+
---
+++ ChangePackBuilder/frmChangePackBuilder.cs
@@ -0,0 +1,142 @@
+﻿#region About
+/*
+ * Written By: Aman Agrawal
+ * First Created On : 15-October-2013
+ */
+#endregion
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Configuration;
+
+namespace ChangePackBuilder
+{
+    public partial class frmChangePackBuilder : Form
+    {
+        private string destinationFolder;
+        private List<string> allowedFileTypes;
+        private List<string> ignoredPaths;
+        List<string> changedFiles;
+
+        public frmChangePackBuilder()
+        {
+            InitializeComponent();            
+            string fileTypesString = ConfigurationManager.AppSettings["SupportedFileTypes"];
+            string ignoredPathString = ConfigurationManager.AppSettings["IgnoredPaths"];
+            allowedFileTypes = fileTypesString.Split(new char[] { ',' }, StringSplitOptions.None).ToList();
+            ignoredPaths = ignoredPathString.Split(new char[] { ',' }, StringSplitOptions.None).ToList();
+            changedFiles = new List<string>();                        
+        }
+                
+        private void btnMonitor_Click(object sender, EventArgs e)
+        {
+            btnMonitor.Enabled = false;
+            changeTracker.Changed += new System.IO.FileSystemEventHandler(changeTracker_Changed);            
+        }        
+
+        void changeTracker_Changed(object sender, System.IO.FileSystemEventArgs e)
+        {
+            if (!changeList.Items.Contains(e.FullPath))
+            {
+                changeList.Items.Add(e.FullPath);
+            }            
+        }
+
+        private void btnBuildReleasePack_Click(object sender, EventArgs e)
+        {                                   
+            //lets pull items out into a list so we can use LINQ and lamdas
+            changedFiles.Clear();
+
+            foreach (string item in changeList.Items)
+            {
+                changedFiles.Add(item);
+            }
+
+            try
+            {
+                //prefix the ignored paths with the path we are monitoring since the project root may vary from machine to machine but
+                //the project folder structure won't and we need to do an exact match against the ignore list
+                ignoredPaths = ignoredPaths.Select(x => txtPathToMonitor.Text + x).ToList();
+                //lets filter out any non-allowed files that might be created/edited/deleted as a result of me changing the files
+                changedFiles = changedFiles.Where(x => allowedFileTypes.Contains(Path.GetExtension(x))).Select(x => x.Split(new char[] { '~' })[0]).ToList();
+
+                foreach (string filePath in changedFiles)
+                {
+                    //dont include any obj, bin or client bin folders as these are runtime folders, also ignore anything in the ignored list
+                    if (!(filePath.Contains("obj") || filePath.Contains("bin") || filePath.Contains("ClientBin") || ignoredPaths.Contains(filePath)))
+                    {                                          
+                        //remove the project root path from the folder string coz I am just interested in the project folders
+                        //for e.g. if the file path is c:\projects\project1\BLL\DataManager.cs, i want to replicate the folder structure
+                        //BLL onwards so I need to remove the root i.e. c:\projects\project1 and then split out the individual folders                        
+                        string finalDest = destinationFolder + Path.GetDirectoryName(filePath).Replace(txtPathToMonitor.Text, "");
+                        Directory.CreateDirectory(finalDest);
+                        File.Copy(filePath, finalDest + "\\" + Path.GetFileName(filePath), true);
+                    }
+                }
+
+                MessageBox.Show("Done!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message + "\r\n" + ex.StackTrace);
+            }
+            finally
+            {
+                btnMonitor.Enabled = true;
+                btnStopMonitoring_Click(this, EventArgs.Empty);
+            }            
+        }
+
+        private void btnSelectPathToMonitor_Click(object sender, EventArgs e)
+        {
+            if (DialogResult.OK == ReleaseFolderBrowser.ShowDialog())
+            {
+                txtPathToMonitor.Text = ReleaseFolderBrowser.SelectedPath;
+                changeTracker.Path = txtPathToMonitor.Text;
+            }
+        }
+
+        private void btnBrowseToReleaseDestination_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(destinationFolder))
+            {
+                if (DialogResult.OK == ReleaseFolderBrowser.ShowDialog())
+                {
+                    destinationFolder = ReleaseFolderBrowser.SelectedPath;
+                    txtReleaseDestination.Text = destinationFolder;
+                }
+            }
+        }
+
+        private void frmReleaseBuilder_Load(object sender, EventArgs e)
+        {
+            this.Text += Application.ProductVersion;
+        }
+
+        private void btnStopMonitoring_Click(object sender, EventArgs e)
+        {            
+            changeTracker.Changed -= new System.IO.FileSystemEventHandler(changeTracker_Changed);
+            btnMonitor.Enabled = true;
+        }
+
+        private void btnClearList_Click(object sender, EventArgs e)
+        {
+            changeList.Items.Clear();
+        }
+
+        private void removeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (changeList.SelectedItem != null)
+            {                
+                changeList.Items.RemoveAt(changeList.SelectedIndex);
+            }            
+        }
+    }
+}
